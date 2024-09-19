e49189d16f999c3c4410f5db1c5d6a7b26250e68
camille823 <camilleconner26@gmail.com>
2024-09-19 12:08:10 -0400

    Troubleshooting RSS

--- src/pages/posts/rss.xml.js
+++ src/pages/posts/rss.xml.js
@@ -1,11 +1,9 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-import frontmatter from './src/layouts/MarkdownPostLayout.astro';
-
 export async function GET(context) {
   return rss({
-    title: frontmatter.title,
-    description: frontmatter.description,
+    title:"Simple Blog",
+    description: "Simple Blog",
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
