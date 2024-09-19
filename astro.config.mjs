ec043f4568db98af713a2d7ae6a9ea650c45c60f
camille823 <camilleconner26@gmail.com>
2024-09-19 11:35:49 -0400

    Added Tags, added Post Title and Description to Home Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import icon from 'astro-icon';
 import { defineConfig } from "astro/config";
  import icon from "astro-icon";
 
+ import { defineConfig } from 'astro/config';
+ import vue from '@astrojs/vue';
+ 
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [icon(),vue()]
+
 });
+
