0f2d1659ec49dc3ce12e1d07aee304f4b91c2e48
camille823 <camilleconner26@gmail.com>
2024-09-20 06:57:53 -0400

    Installing Strapi CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+
 import icon from 'astro-icon';
 import vue from '@astrojs/vue';
 import react from '@astrojs/react';
-import storyblok from '@storyblok/astro'
 
 import pagefind from "astro-pagefind";
 
 
 export default defineConfig({
-  integrations: [icon(), vue(), react(),pagefind(),
-    storyblok({
-    accessToken: 'tV65xUbymlZ0UJKT1Wcjmgtt',
-    components: {
-      page: 'storyblok/Page',
-      feature: 'storyblok/Feature',
-      grid: 'storyblok/Grid',
-      teaser: 'storyblok/Teaser',
-    },
-  }),],
+  integrations: [icon(), vue(), react(),pagefind()],
 
   site: "https://extraordinary-unicorn-841d05.netlify.app"
 });
