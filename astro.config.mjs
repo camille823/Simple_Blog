0229fbbe696b56be0255e5366d13edc3a5294319
camille823 <camilleconner26@gmail.com>
2024-09-20 04:47:45 -0400

    Cleaning up my files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 import { defineConfig } from 'astro/config';
 
-import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 import vue from '@astrojs/vue';
 import react from '@astrojs/react';
+import storyblok from '@storyblok/astro'
 
 import pagefind from "astro-pagefind";
 
 
 export default defineConfig({
-  integrations: [icon(), vue(), mdx(), react(),pagefind()],
+  integrations: [icon(), vue(), react(),pagefind(),
+    storyblok({
+    accessToken: 'tV65xUbymlZ0UJKT1Wcjmgtt',
+    components: {
+      page: 'storyblok/Page',
+      feature: 'storyblok/Feature',
+      grid: 'storyblok/Grid',
+      teaser: 'storyblok/Teaser',
+    },
+  }),],
 
   site: "https://extraordinary-unicorn-841d05.netlify.app"
 });
+
